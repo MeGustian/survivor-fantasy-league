6065abc1db573cdd4a49ff58fdf8a502032d505e
MeGustian <yoro187@gmail.com>
2015-09-21 20:44:03 +0300

    merging codes together

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 // TODO: Remove unecessary packages from package.json (including all the other
 // scripts).
-var fs = require('fs'),
-    path = require('path'),
-    express = require('express'),
-    stylus = require('stylus'),
-    nib = require('nib'),
-    bodyParser = require('body-parser');
-// A lot of express in the server side. I cannot even tell which functions are
-// part of express and which are just vanilla node.
-var app = module.exports = express();
+var fs = require('fs');
+var stylus = require('stylus');
+var nib = require('nib');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/survivor');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var admin = require('./routes/admin');
+
+var app = express();
 
 // The name of the dummy JSON file.
 // var jsonFileName = 'possibles';
-
-// TODO: Understand this. Also see app.listen below.
-app.set('port', (process.env.PORT || 3000));
 
 function compile(str, path) {
   return stylus(str)
@@ -44,72 +52,46 @@ app.use(bodyParser.urlencoded({extended: true}));
 // TODO: Understand this.
 // app.use(app.router);
 
-// TODO: Handle errors in the HTTP requests.
-// Performs a GET HTTP request, and (I think) calls the function when done.
-// This one renders `/views/home.jade` with sayHelloTo ready to intepolate.
-app.get('/', function(req, res){
-  res.render('home', {sayHelloTo: 'world'});
-});
-// TODO: Understand this.
-// app.get('/' + jsonFileName + '.json', function(req, res) {
-//   fs.readFile(jsonFileName + '.json', function(err, data) {
-//     res.setHeader('Cache-Control', 'no-cache');
-//     res.json(JSON.parse(data));
-//   });
-// });
 
-// Performs a POST HTTP request, and (I think) calls the function when done.
-// This one adds data to the JSON file.
-// app.post('/' + jsonFileName + '.json', function(req, res) {
-//   fs.readFile(jsonFileName + '.json', function(err, data) {
-//     var comments = JSON.parse(data);
-//     comments.push(req.body);
-//     fs.writeFile(jsonFileName + '.json', JSON.stringify(comments, null, 4), function(err) {
-//       res.setHeader('Cache-Control', 'no-cache');
-//       res.json(comments);
-//     });
-//   });
-// });
-
-// TODO: Deal with booleans (they are returned as strings!).
-app.get('/ajax', function (req, res) {
-  console.log('GET:');
-  console.log(req.query);
-  if (true) {
-    res.status(200).send(req.query);
-  }
-  if (!true) {
-    res.status(500).send({error: req.query});
-  }
-  // timeout...
-});
-// TODO: Deal with booleans (they are returned as strings!).
-app.post('/ajax', function (req, res) {
-  console.log('POST:');
-  console.log(req.body);
-  if (true) {
-    res.status(200).send(req.body);
-  }
-  if (!true) {
-    res.status(500).send({error: req.body});
-  }
-  // timeout...
-});
-app.post('/sign-in', function (req, res) {
-  console.log('sign-in');
-  console.log(req.body);
-  if (true) {
-    res.status(200).send(req.body);
-  }
-  if (!true) {
-    res.status(500).send({error: req.body});
-  }
-  // timeout...
+app.use(function(req,res,next){
+  req.db = db;
+  next();
 });
 
-// TODO: Understand this.
-if (!module.parent) {
-  app.listen(app.get('port'), function() {
-    console.log('up and running');
+app.use('/', routes);
+app.use('/users', users);
+app.use('/admin', admin);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
