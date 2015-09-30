74de0938ce828c61a064ddf6677ea2041897d509
yotamDvir <rocky_2@Yotams-MacBook-Air.local>
2015-09-30 16:42:06 +0300

    runs on safari
    also doesn’t redirect to nonexistent error.jade

--- public/bundle.js
+++ public/bundle.js
@@ -60006,11 +60006,11 @@ var questions = function (prev, action) {
 };
 
 var reducers = combineReducers({
-	controller,
-	navigation,
-	profile,
-	contestants,
-	questions
+	controller: controller,
+	navigation: navigation,
+	profile: profile,
+	contestants: contestants,
+	questions: questions
 });
 
 var truthiness = function (bool) {
