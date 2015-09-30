f04c55b4b8911174d4f3112d18fd17273924e585
yotamDvir <rocky_2@Yotams-MacBook-Air.local>
2015-09-30 15:26:58 +0300

    works locally with db

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 // TODO: Remove unecessary packages from package.json (including all the other
 // scripts).
-var fs = require('fs'),
-path = require('path'),
-express = require('express'),
-stylus = require('stylus'),
-nib = require('nib'),
-bodyParser = require('body-parser');
-// A lot of express in the server side. I cannot even tell which functions are
-// part of express and which are just vanilla node.
-var app = module.exports = express();
+var fs = require('fs');
+var stylus = require('stylus');
+var nib = require('nib');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
 
-// The name of the dummy JSON file.
-// var jsonFileName = 'possibles';
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var mongoose = require('mongoose');
+var db = monk('survivoradmin:survivorpass@ds051853.mongolab.com:51853/survivor-fantasy-league');
 
-// TODO: Understand this. Also see app.listen below.
-app.set('port', (process.env.PORT || 3000));
+var routes = require('./routes/index');
+
+var app = express();
+
+// ==============CONFIGURARTIONS=============================
+
+mongoose.connect('survivoradmin:survivorpass@ds051853.mongolab.com:51853/survivor-fantasy-league');
+require('./config/passport')(passport); // pass passport for configuration
+//require('./routes/index.js')(passport);
 
 function compile(str, path) {
 	return stylus(str)
-	.set('filename', path)
-	.use(nib())
-	.import('nib');
+		.set('filename', path)
+		.use(nib())
+		.import('nib');
 }
 // Set views with Jade. Read below to understand the __dirname thing.
 app.set('views', __dirname + '/views');
@@ -28,11 +41,11 @@ app.set('view engine', 'jade');
 // Using Stylus with nib.
 app.use(stylus.middleware(
 	{ src: __dirname + '/public'
-	, compile: compile
-}
+		, compile: compile
+	}
 ));
 
-// Set `/public` as the static resources, which is accessable from the browser
+// Set `/public` as the static resources, which is accessible from the browser
 // as if public was the root directory.
 // Example: If I want to access `/public/images/logo.png`, I could write
 // `__full-site-URL__/images/logo.png`.
@@ -43,545 +56,60 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // TODO: Understand this.
 // app.use(app.router);
+app.use(cookieParser()); // read cookies (needed for auth)
+// required for passport
+app.use(session({ secret: 'thisisasecrettest' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-// TODO: Handle errors in the HTTP requests.
-// Performs a GET HTTP request, and (I think) calls the function when done.
-// This one renders `/views/home.jade` with sayHelloTo ready to intepolate.
-app.get('/', function(req, res){
-	res.render('home', {sayHelloTo: 'world'});
-});
-// TODO: Understand this.
-// app.get('/' + jsonFileName + '.json', function(req, res) {
-//	 fs.readFile(jsonFileName + '.json', function(err, data) {
-//		 res.setHeader('Cache-Control', 'no-cache');
-//		 res.json(JSON.parse(data));
-//	 });
-// });
+// Connect to DB
+app.use(function(req,res,next){
+	req.db = db;
+	next();
 
-// Performs a POST HTTP request, and (I think) calls the function when done.
-// This one adds data to the JSON file.
-// app.post('/' + jsonFileName + '.json', function(req, res) {
-//	 fs.readFile(jsonFileName + '.json', function(err, data) {
-//		 var comments = JSON.parse(data);
-//		 comments.push(req.body);
-//		 fs.writeFile(jsonFileName + '.json', JSON.stringify(comments, null, 4), function(err) {
-//			 res.setHeader('Cache-Control', 'no-cache');
-//			 res.json(comments);
-//		 });
-//	 });
-// });
-
-// TODO: Deal with booleans (they are returned as strings!).
-app.get('/ajax', function (req, res) {
-	console.log('GET:');
-	console.log(req.query);
-	if (true) {
-		res.status(200).send(req.query);
-	}
-	if (!true) {
-		res.status(500).send({error: req.query});
-	}
-	// timeout...
-});
-// TODO: Deal with booleans (they are returned as strings!).
-app.post('/ajax', function (req, res) {
-	console.log('POST:');
-	console.log(req.body);
-	if (true) {
-		res.status(200).send(req.body);
-	}
-	if (!true) {
-		res.status(500).send({error: req.body});
-	}
-	// timeout...
-});
-var week1 = {
-	"username":"yoav",
-	"isAdmin":"false",
-	questions: {
-		"124": {
-			question: 'Bool?',
-			type: 'boolean'
-		},
-		"125": {
-			question: 'Cont?',
-			type: 'contestant'
-		}
-	},
-	"contestantStatus":{
-		"56029683b2c3b5601f976290":{
-			"_id":"5602a0cce855f3dc1fd1303b",
-			"contestantId":"56029683b2c3b5601f976290",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-				"CRIED": true,
-				"HASHTAG": true
-			}
-		},
-		"560296b7b2c3b5601f976291":{
-			"_id":"5602a0e8e855f3dc1fd1303c",
-			"contestantId":"560296b7b2c3b5601f976291",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-				"CRIED": true,
-				"HASHTAG": true
-			}
-		},
-		"560296e1b2c3b5601f976292":{
-			"_id":"5602a0f1e855f3dc1fd1303d",
-			"contestantId":"560296e1b2c3b5601f976292",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-				"CRIED": true,
-				"HASHTAG": true,
-				"TREE-MAIL": true
-			}
-		},
-		"56029707b2c3b5601f976293":{
-			"_id":"5602a0f8e855f3dc1fd1303e",
-			"contestantId":"56029707b2c3b5601f976293",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029724b2c3b5601f976294":{
-			"_id":"5602a101e855f3dc1fd1303f",
-			"contestantId":"56029724b2c3b5601f976294",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-				"CRIED": true,
-				"HASHTAG": true
-			}
-		},
-		"56029749b2c3b5601f976295":{
-			"_id":"5602a109e855f3dc1fd13040",
-			"contestantId":"56029749b2c3b5601f976295",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-				"CRIED": true,
-				"HASHTAG": true,
-				"TREE-MAIL": true
-			}
-		},
-		"560298ceb2c3b5601f976296":{
-			"_id":"5602a116e855f3dc1fd13041",
-			"contestantId":"560298ceb2c3b5601f976296",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"560298e8b2c3b5601f976297":{
-			"_id":"5602a123e855f3dc1fd13042",
-			"contestantId":"560298e8b2c3b5601f976297",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029919b2c3b5601f976298":{
-			"_id":"5602a12ae855f3dc1fd13043",
-			"contestantId":"56029919b2c3b5601f976298",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029937b2c3b5601f976299":{
-			"_id":"5602a133e855f3dc1fd13044",
-			"contestantId":"56029937b2c3b5601f976299",
-			"tribe":"Ta-Keo",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"5602994cb2c3b5601f97629a":{
-			"_id":"5602a148e855f3dc1fd13045",
-			"contestantId":"5602994cb2c3b5601f97629a",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"5602996ab2c3b5601f97629b":{
-			"_id":"5602a154e855f3dc1fd13046",
-			"contestantId":"5602996ab2c3b5601f97629b",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029983b2c3b5601f97629c":{
-			"_id":"5602a15de855f3dc1fd13047",
-			"contestantId":"56029983b2c3b5601f97629c",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"5602999db2c3b5601f97629d":{
-			"_id":"5602a163e855f3dc1fd13048",
-			"contestantId":"5602999db2c3b5601f97629d",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"560299c2b2c3b5601f97629e":{
-			"_id":"5602a16ae855f3dc1fd13049",
-			"contestantId":"560299c2b2c3b5601f97629e",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"560299e5b2c3b5601f97629f":{
-			"_id":"5602a171e855f3dc1fd1304a",
-			"contestantId":"560299e5b2c3b5601f97629f",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029a03b2c3b5601f9762a0":{
-			"_id":"5602a17ae855f3dc1fd1304b",
-			"contestantId":"56029a03b2c3b5601f9762a0",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029a1eb2c3b5601f9762a1":{
-			"_id":"5602a180e855f3dc1fd1304c",
-			"contestantId":"56029a1eb2c3b5601f9762a1",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029a3db2c3b5601f9762a2":{
-			"_id":"5602a186e855f3dc1fd1304d",
-			"contestantId":"56029a3db2c3b5601f9762a2",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		},
-		"56029a55b2c3b5601f9762a3":{
-			"_id":"5602a18be855f3dc1fd1304e",
-			"contestantId":"56029a55b2c3b5601f9762a3",
-			"tribe":"Bayon",
-			"votedFor":"",
-			"achievements":{
-
-			}
-		}
-	},
-	"weekNumber":1,
-	"allContestants":{
-		"56029683b2c3b5601f976290":{
-			"_id":"56029683b2c3b5601f976290",
-			"firstName":"Abi-Maria",
-			"lastName":"Gomes",
-			"age":"35",
-			"occupation":"Business Student",
-			"previousSeason":"Philippines",
-			"place":"5/18"
-		},
-		"560296b7b2c3b5601f976291":{
-			"_id":"560296b7b2c3b5601f976291",
-			"firstName":"Jeff",
-			"lastName":"Varner",
-			"age":"49",
-			"occupation":"Internet Projects Manager",
-			"previousSeason":"The Australian Outback",
-			"place":"10/16"
-		},
-		"560296e1b2c3b5601f976292":{
-			"_id":"560296e1b2c3b5601f976292",
-			"firstName":"Kelley",
-			"lastName":"Wentworth",
-			"age":"29",
-			"occupation":"Marketing Manager",
-			"previousSeason":"San Juan del Sur",
-			"place":"14/18"
-		},
-		"56029707b2c3b5601f976293":{
-			"_id":"56029707b2c3b5601f976293",
-			"firstName":"Kelly",
-			"lastName":"Wiglesworth",
-			"age":"37",
-			"occupation":"River guide",
-			"previousSeason":"Borneo",
-			"place":"2/16"
-		},
-		"56029724b2c3b5601f976294":{
-			"_id":"56029724b2c3b5601f976294",
-			"firstName":"Peih-Gee",
-			"lastName":"Law",
-			"age":"37",
-			"occupation":"Jeweler",
-			"previousSeason":"China",
-			"place":"5/16"
-		},
-		"56029749b2c3b5601f976295":{
-			"_id":"56029749b2c3b5601f976295",
-			"firstName":"Shirin",
-			"lastName":"Oskooi",
-			"age":"32",
-			"occupation":"Yahoo! Executive",
-			"previousSeason":"Worlds Apart",
-			"place":"8/18"
-		},
-		"560298ceb2c3b5601f976296":{
-			"_id":"560298ceb2c3b5601f976296",
-			"firstName":"Spencer",
-			"lastName":"Bledsoe",
-			"age":"22",
-			"occupation":"Economics Students",
-			"previousSeason":"Cagayan",
-			"place":"4/18"
-		},
-		"560298e8b2c3b5601f976297":{
-			"_id":"560298e8b2c3b5601f976297",
-			"firstName":"Terry",
-			"lastName":"Deitz",
-			"age":"55",
-			"occupation":"Pilot",
-			"previousSeason":"Panama",
-			"place":"3/16"
-		},
-		"56029919b2c3b5601f976298":{
-			"_id":"56029919b2c3b5601f976298",
-			"firstName":"Vytas",
-			"lastName":"Baskauskas",
-			"age":"35",
-			"occupation":"Yoga Instructor/Math Professor",
-			"previousSeason":"Blood vs. Water",
-			"place":"10/20"
-		},
-		"56029937b2c3b5601f976299":{
-			"_id":"56029937b2c3b5601f976299",
-			"firstName":"Woo",
-			"lastName":"Hwang",
-			"age":"31",
-			"occupation":"Martial Arts Instructor",
-			"previousSeason":"Cagayan",
-			"place":"2/18"
-		},
-		"5602994cb2c3b5601f97629a":{
-			"_id":"5602994cb2c3b5601f97629a",
-			"firstName":"Andrew",
-			"lastName":"Savage",
-			"age":"51",
-			"occupation":"Attorney",
-			"previousSeason":"Pearl Islands",
-			"place":"10/16"
-		},
-		"5602996ab2c3b5601f97629b":{
-			"_id":"5602996ab2c3b5601f97629b",
-			"firstName":"Ciera",
-			"lastName":"Eastin",
-			"age":"26",
-			"occupation":"Cosmetology Student",
-			"previousSeason":"Blood vs. Water",
-			"place":"5/20"
-		},
-		"56029983b2c3b5601f97629c":{
-			"_id":"56029983b2c3b5601f97629c",
-			"firstName":"Jeremy",
-			"lastName":"Collins",
-			"age":"37",
-			"occupation":"Firefighter",
-			"previousSeason":"San Juan del Sur",
-			"place":"10/18"
-		},
-		"5602999db2c3b5601f97629d":{
-			"_id":"5602999db2c3b5601f97629d",
-			"firstName":"Joe",
-			"lastName":"Anglim",
-			"age":"26",
-			"occupation":"Jewelry Designer",
-			"previousSeason":"Worlds Apart",
-			"place":"10/18"
-		},
-		"560299c2b2c3b5601f97629e":{
-			"_id":"560299c2b2c3b5601f97629e",
-			"firstName":"Kass",
-			"lastName":"McQuillen",
-			"age":"43",
-			"occupation":"Attorney",
-			"previousSeason":"Cagayan",
-			"place":"3/18"
-		},
-		"560299e5b2c3b5601f97629f":{
-			"_id":"560299e5b2c3b5601f97629f",
-			"firstName":"Keith",
-			"lastName":"Nale",
-			"age":"54",
-			"occupation":"Fire Captain",
-			"previousSeason":"San Juan del Sur",
-			"place":"4/18"
-		},
-		"56029a03b2c3b5601f9762a0":{
-			"_id":"56029a03b2c3b5601f9762a0",
-			"firstName":"Kimmi",
-			"lastName":"Kappenberg",
-			"age":"42",
-			"occupation":"Bartender",
-			"previousSeason":"The Australian Outback",
-			"place":"12/16"
-		},
-		"56029a1eb2c3b5601f9762a1":{
-			"_id":"56029a1eb2c3b5601f9762a1",
-			"firstName":"Monica",
-			"lastName":"Padilla",
-			"age":"30",
-			"occupation":"Law Student",
-			"previousSeason":"Samoa",
-			"place":"7/20"
-		},
-		"56029a3db2c3b5601f9762a2":{
-			"_id":"56029a3db2c3b5601f9762a2",
-			"firstName":"Stephen",
-			"lastName":"Fishbach",
-			"age":"36",
-			"occupation":"Corporate Consultant",
-			"previousSeason":"Tocantins",
-			"place":"2/16"
-		},
-		"56029a55b2c3b5601f9762a3":{
-			"_id":"56029a55b2c3b5601f9762a3",
-			"firstName":"Tasha",
-			"lastName":"Fox",
-			"age":"39",
-			"occupation":"Accountant",
-			"previousSeason":"Cagayan",
-			"place":"6/18"
-		}
-	}
-}
-var week = function (req, number) {
-	return {
-		username: 'yoav',
-		isAdmin: 'false',
-		questions: {
-			"124": {
-				question: 'Bool?',
-				type: 'boolean'
-			},
-			"125": {
-				question: 'Cont?',
-				type: 'contestant'
-			}
-		},
-		contestantStatus: {
-			"1": {
-				tribe: "Abu",
-				votedFor: "3",
-				achievements: {
-					"CRIED": true,
-					"HASHTAG": false
-				}
-			},
-			"7": {
-				tribe: "Abu",
-				votedFor: "2",
-				achievements: {
-					"TREE-MAIL": true,
-					"HASHTAG": true
-				}
-			},
-			"2": {
-				tribe: "Abu",
-				votedFor: "3",
-				achievements: {
-					"CRIED": true,
-					"HASHTAG": false,
-					"TREE-MAIL": true
-				}
-			},
-			"3": {
-				tribe: "Abu",
-				votedFor: "1",
-				achievements: {
-					"CRIED": true,
-					"HASHTAG": false
-				}
-			},
-			"4": {
-				tribe: "Bala",
-				votedFor: "5",
-				achievements: {
-					"CRIED": true,
-					"HASHTAG": false
-				}
-			},
-			"5": {
-				tribe: "Bala",
-				votedFor: "4",
-				achievements: {
-					"CRIED": true,
-					"HASHTAG": false
-				}
-			},
-			"6": {
-				tribe: "Bala",
-				votedFor: "4",
-				achievements: {
-					"HASHTAG": false
-				}
-			}
-		},
-		weekNumber: number,
-		allContestants: {
-			"1": {name: "Spencer"},
-			"2": {name: "Kass"},
-			"3": {name: "Jonny"},
-			"4": {name: "Adam"},
-			"5": {name: "Tom"},
-			"6": {name: "Parvati"},
-			"7": {name: "Sandra"}
-		}
-	};
-};
-app.get('/initial', function (req, res) {
-	if (true) {
-		res.status(200).send(week1);
-	}
-	if (!true) {
-		res.status(500).send({error: req.body});
-	}
-	// timeout...
-});
-app.post('/v/1', function (req, res) {
-	console.log('post v1');
-	res.status(200).send(req.body);
 });
 
-// TODO: Understand this.
-if (!module.parent) {
-	app.listen(app.get('port'), function() {
-		console.log('up and running');
+
+app.use('/', routes);
+
+
+
+
+// ==============END CONFIGURARTIONS=============================
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
 	});
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+
+module.exports = app;
